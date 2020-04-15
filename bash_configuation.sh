b3440f6a1ea8147aa0a78f09198c2bd3bbf9c708
gregoire <gregoire.pichereau@imt-atlantique.net>
2020-04-15 20:40:13 +0200

    add bash file and connexion launch file

---
+++ bash_configuation.sh
@@ -0,0 +1,13 @@
+
+
+# source catkin into terminal
+
+## uncomment this line if you use a python virtual environment
+#source <path of your virtualenv folder>/bin/activate
+
+## change this line to source the bash file of your catkin workspace
+#source <path to your catkin directory>/devel/setup.bash
+
+## those lines are used for the connexion with the robot
+export ROS_MASTER_URI=http://192.168.12.1:11311
+export ROS_IP=192.168.12.95
